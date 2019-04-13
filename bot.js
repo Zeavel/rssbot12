17b95aee34c1d558d17b1b0c1ebb2da4a8daff3f
Zeavel <34531943+Zeavel@users.noreply.github.com>
2019-04-13 22:57:58 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const CC = require('./command_create.js');
-const Command = CC.Command;
 
 
 
-var Commandss = new CC.Commands();
 var fs = require("fs");
 
 function commandIs(str, msg){
@@ -28,68 +25,110 @@ function hasRole(mem, role)
         return false;
     }
 }
-client.on("ready", r=>{
-    function sec() {
-       
-        
-        var cheerio = require('cheerio');
-        var request = require('request');
-      var urle = "https://squad-servers.com/server/7507/"
-        request(urle, function (error, response, body) {
-          if (!error) {
-              var $ = cheerio.load(body)
-              var status = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-7 > table > tbody > tr:nth-child(3) > td:nth-child(2) > button").text()
-              var players = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-7 > table > tbody > tr:nth-child(4) > td:nth-child(2) > strong").text().trim()
-              if(status == "Online")
-              {
-                  client.user.setActivity(players + " players", { type: "PLAYING"})
-                 /* client.user.setPresence({ game: { name: players }, status: 'online' })
-                  .then(console.log)
-                  .catch(console.error);*/
-              }
-              if(status == "Offline")
-              {
-                client.user.setPresence({status: 'dnd', activity: {name: "Offline"}})
-                
-              }
-        }})
-        }
-        
-        setInterval(sec, 5000)
-   
-})
-client2.on("ready", r=>{
-    function sec() {
-       
-        
-        var cheerio = require('cheerio');
-        var request = require('request');
-      var urle = "https://squad-servers.com/server/7998/"
-        request(urle, function (error, response, body) {
-          if (!error) {
-              var $ = cheerio.load(body)
-              var status = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-7 > table > tbody > tr:nth-child(3) > td:nth-child(2) > button").text()
-              var players = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-7 > table > tbody > tr:nth-child(4) > td:nth-child(2) > strong").text().trim()
-              if(status == "Online")
-              {
-                  client2.user.setActivity(players + " players", { type: "PLAYING"})
-                 /* client.user.setPresence({ game: { name: players }, status: 'online' })
-                  .then(console.log)
-                  .catch(console.error);*/
-              }
-              if(status == "Offline")
-              {
-                client2.user.setPresence({status: 'dnd', activity: {name: "Offline"}})
-                
-              }
-        }})
-        }
-        
-        setInterval(sec, 5000)
-   
-})
+
 
 client.on('message', message => {
+    if(message.content.includes("-rss"))
+    {
+        var url = message.content.substring(5)
+        console.log(url)
+        var cheerio = require('cheerio');
+        var request = require('request');
+
+        request(url, function (error, response, body) {
+          if (!error) {
+              
+            var $ = cheerio.load(body)
+            var name = $("body > div.content > div > div:nth-child(3) > div > h1").text()
+         
+           
+       
+    
+message.channel.send("-apl "+name)
+        
+        
+        
+        
+      
+        }})
+    }
+    if(message.content.includes("info"))
+    {
+       
+        var prefix = message.content.split("info")[0]
+        if(prefix != "")
+        {
+            client.guilds.get("351491707554103296").channels.get("564800728061706261").fetchMessage("565411651344400384")
+            .then(ms=>{
+if(ms.content.includes(prefix))
+{
+    var num = ms.content.split("darud").indexOf(prefix)
+    client.guilds.get("351491707554103296").channels.get("564800728061706261").fetchMessage("565411537486086144")
+    .then(mse=>{
+var url = mse.content.split("darud")[num]
+var cheerio = require('cheerio');
+var request = require('request');
+console.log(url)
+request(url, function (error, response, body) {
+  if (!error) {
+      
+    var $ = cheerio.load(body)
+    var name = $("body > div.content > div > div:nth-child(3) > div > h1").text()
+    var status = $("body > div.content > div > div:nth-child(3) > div > div.row > div.col-12.col-md-7 > table > tbody > tr:nth-child(3) > td:nth-child(2) > button").text()
+    var playerss = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-7 > table > tbody > tr:nth-child(4) > td:nth-child(2) > strong").text().trim()
+    var map = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-7 > table > tbody > tr:nth-child(8) > td:nth-child(2) > strong").text()
+    var regsin = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-7 > table > tbody > tr:nth-child(9) > td:nth-child(2)").text()
+     var mapimg;
+    var mapimg2;
+   console.log(body)
+    if($("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-5 > div:nth-child(3) > img").attr("src") != undefined)
+    {
+        mapimg = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-5 > div:nth-child(3) > img").attr("src").replace(/[']/g, "%27").replace(/[ ]/g,"%20")
+        mapimg2= "https://squad-servers.com"+mapimg  
+    }
+    else
+    {
+        mapimg2 = ''
+    }
+    var players = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(6) > div > div").text()//.replace(/[,]/g,"\n")
+    var location = $("body > div.content > div > div:nth-child(3) > div > div:nth-child(4) > div.col-12.col-md-7 > table > tbody > tr:nth-child(5) > td:nth-child(2) > a").text()
+   var color;
+   if(status === "Online")
+   {
+       color = "#00FF00"
+   }
+   else
+   {
+       color = "#FF0000"
+   }
+
+    const Discord = require('discord.js')
+    const embed = new Discord.RichEmbed()
+    .setThumbnail("https://cdn.discordapp.com/attachments/486990358455123978/523920686570405898/2_2.png")
+    .setTitle(name)
+    .setImage(mapimg2)
+    
+    .addField("Players", "**"+playerss+"**")
+    .addField("Status", "**"+status+"**")
+    .addField("Location", "**"+location+"**")
+    .addField("Map", "**"+map+"**")
+    .addField("Registered since", "**"+regsin+"**")
+    .setColor(color)
+    .setFooter(players)
+    message.channel.send({embed})
+
+
+
+
+
+}})
+    })
+}
+            })
+          
+        }
+      
+    }
    if(commandIs("info-", message))
    { 
     
@@ -217,7 +256,3 @@ client.on('message', message => {
   });
 
 client.login(process.env.BOT_TOKEN);
-client2.login(process.env.BOT_TOKEN2);
-
-
-
